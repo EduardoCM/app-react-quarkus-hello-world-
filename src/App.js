6cd869c751440b348da9b87f97a6a9924ecd723e
Eduardo Castillo <ecastillo@anzen.com.mx>
2022-10-01 12:53:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [nombre, asignarNombre] = useState('');
+
+  const [saludo, asignarSaludo] = useState('Sin recibir respuesta a saludo');
+
+  function evtInputText(e){
+    asignarNombre(e.target.value);
+  }
+
+  const saludar = () => {
+    console.log("Bienvenido Quarkus a React " + nombre);
+
+    axios.get('http://localhost:8080/hello/' + nombre)
+    .then(resultado => {
+      console.log('Resultado: ' + resultado.data);
+      asignarSaludo(resultado.data);
+    }).catch(error => {
+      console.log(error);
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Creando mi primer plataforma digital con React y Quarkus</h1>
+      <br/>
+      Ingresa tu nombre: <input name="nombre" type="text" value={nombre} onChange={evtInputText} />
+      <br/>
+      <button onClick={() => saludar()}> Saludar a Quarkus </button>
+
+      <br/>
+      <strong>{saludo}</strong>
+
+
     </div>
   );
 }
---
+++ src/componentes/RegistroTarjeta.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+
+
+class RegistroTarjeta extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardHolderName: "",
+            creditCardNumber: "",
+            expirationDate: "",
+            paymentNetworks: "",
+            cvv: ""
+        }
+    }
+
+    registrarTarjeta = (e) => {
+        e.preventDefault();
+        console.log("Se registra tarjeta");
+        console.log(this.state);
+
+        fetch('http://localhost:8080/api/creditcards', {
+            method: 'POST',
+            body: JSON.stringify({
+                cardHolderName: this.state.cardHolderName,
+                creditCardNumber: this.state.creditCardNumber,
+                cvv: this.state.cvv,
+                expirationDate: this.state.expirationDate,
+                paymentNetworks: this.state.paymentNetworks
+            }),
+            headers: {
+                "Content-type": "Application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+            console.log("Creacion de tarjeta de forma correcta");
+            console.log(json)
+        })
+    }
+
+
+    render (){
+        return (
+            
+            <form onSubmit={this.registrarTarjeta}>
+                <h1>Registrar tarjeta de Credito</h1>
+                <div>
+                    <label>Card Holder Name</label>
+                    <input type="text" 
+                           id="name" 
+                           value={this.state.cardHolderName} 
+                           onChange={ev => this.setState({cardHolderName: ev.target.value})}/>
+
+                </div>
+
+                <div>
+                    <label>Credit Card Number </label>
+                    <input type="text" 
+                           id="cardNumber" 
+                           value={this.state.creditCardNumber} 
+                           onChange={ev => this.setState({creditCardNumber: ev.target.value})}/>
+
+                </div>
+
+                <div>
+                    <label>Expiration Date</label>
+                    <input type="text" 
+                           id="expiracion" 
+                           value={this.state.expirationDate} 
+                           onChange={ev => this.setState({expirationDate: ev.target.value})}/>
+                </div>
+
+                <div>
+                    <label>Payment Networks</label>
+                    <input type="text" 
+                           id="paymentNetwork" 
+                           value={this.state.paymentNetworks} 
+                           onChange={ev => this.setState({paymentNetworks: ev.target.value})}/>
+
+                </div>
+
+                <div>
+                    <label>Cvv</label>
+                    <input type="text" 
+                           id="expiracion" 
+                           value={this.state.cvv} 
+                           onChange={ev => this.setState({cvv: ev.target.value})}/>
+
+                </div>
+
+                <input type="submit" value="Enviar" />
+            </form>
+        )
+    }
+
+}
+
+
+export default RegistroTarjeta;
